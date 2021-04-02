5fe39cc51429c1ae2825554878a90fa22ae68cff
dingusbr <64748785+dingusbr@users.noreply.github.com>
2021-04-01 21:04:35 -0400

    Update HazeApp.java

--- HazeApp.java
+++ HazeApp.java
@@ -3,46 +3,53 @@ import java.awt.*;
 import java.sql.SQLException;
 
 public class HazeApp {
-    public static final JFrame frame = new JFrame("HazeApp");
     public static final SqlServerConnection conn  = new SqlServerConnection();
+    public static JLabel userLabel;
+    public static JTextField userText;
+    public static JLabel pwLabel;
+    public static JPasswordField pwText;
+    public static JButton signInbutton;
+    public static JButton signUpButton;
 
     public static void main(String[] args) throws SQLException {
-
-
-        JFrame.setDefaultLookAndFeelDecorated(true);
-
-
-
-        JTextField jtf = new JTextField("Username");
-        JTextField jpwf = new JTextField("Password");
-        JButton logInBttn = new JButton("Sign in");
-        JLabel lbl = new JLabel("Don't have an account yet?");
-        JButton signUpBttn = new JButton("Sign up");
-        JPanel jp1 = new JPanel();
-        JPanel jp2 = new JPanel();
-
-
-        jp1.setLayout(new FlowLayout());
-        jp2.setLayout(new FlowLayout());
-        frame.setLayout(new BorderLayout());
-        frame.add(jp1, BorderLayout.NORTH);
-        frame.add(jp2, BorderLayout.CENTER);
-
-        jp1.add(jtf);
-        jp1.add(jpwf);
-        jp1.add(logInBttn);
-        jp2.add(lbl);
-        jp2.add(signUpBttn);
+    	
+    	JPanel panel = new JPanel();
+    	JFrame frame = new JFrame();
+    	frame.setSize(720,640);
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.add(panel);
+    	
+    	panel.setLayout(null);
+    	
+    	JLabel userLabel = new JLabel("User:");
+    	userLabel.setBounds(10, 20, 80, 25);
+    	panel.add(userLabel);
+    	
+    	JTextField userText = new JTextField(20);
+    	userText.setBounds(50, 20, 165, 25);
+    	panel.add(userText);
+    	
+    	JLabel pwLabel = new JLabel("Password:");
+    	pwLabel.setBounds(230, 20, 80, 25);
+    	panel.add(pwLabel);
+    	
+    	JPasswordField pwText = new JPasswordField(20);
+    	pwText.setBounds(300, 20, 165, 25);
+    	panel.add(pwText);
+    	
+    	JButton signInButton = new JButton("Sign in");
+    	signInButton.setBounds(480, 20, 80, 25);
+    	panel.add(signInButton);
+    	JButton signUpButton = new JButton("Sign up");
+    	signUpButton.setBounds(570, 20, 80, 25);
+    	panel.add(signUpButton);
+    
         // add the list of apps to the jframe
         JPanel appList = new JPanel();
         frame.add(appList);
         String[] listOfApps = conn.getApps();
         appList.add(new JList<String>(listOfApps));
 
-
-
-        frame.setSize(600, 400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
     }
