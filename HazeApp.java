d2a6d9fd61a89f38f24b4065cc15e48167c3da91
Qi Feng <49265120+Rivendell-FQ@users.noreply.github.com>
2021-04-02 16:22:33 -0400

    username and password update
    username and password update

--- HazeApp.java
+++ HazeApp.java
@@ -1,82 +1,146 @@
+package Hazemain;
+
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+ 
+
+
+//import Haze.HazeApp;
 
 public class HazeApp {
-    public static final SqlServerConnection conn  = new SqlServerConnection();
-    // we make global variables for all things JFrame, so we can access these variables outside of the class
-    public static JPanel panel;
-    public static JFrame frame;
-    public static JTextField userText;
-    public static JPasswordField pwText;
-    public static JButton signInButton;
-    public static JButton signUpButton;
-    public static JTextArea appDesc;
+    public static final JFrame frame = new JFrame("HazeApp");
+    //public static final SqlServerConnection conn  = new SqlServerConnection();
+	//public static final String users = null;
+    Map<String, String> users = new HashMap<>();
+	public User currentUser = new User("","");
 
-    public static void main(String[] args) throws SQLException, SQLException {
+    public static void main(String[] args) throws SQLException {
 
-        panel = new JPanel();
-        frame = new JFrame();
-        frame.setSize(720,640);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(panel);
+    	
+		 User a1 = new User("Username", "Password");
+		 HazeApp AOT = new HazeApp();
 
-        panel.setLayout(null);
-
-        // username label
-        JLabel userLabel = new JLabel("Username:");
-        userLabel.setBounds(10, 20, 80, 25);
-        panel.add(userLabel);
-
-        // username entry field
-        userText = new JTextField(20);
-        userText.setBounds(80, 20, 165, 25);
-        panel.add(userText);
-
-        // password label
-        JLabel pwLabel = new JLabel("Password:");
-        pwLabel.setBounds(250, 20, 80, 25);
-        panel.add(pwLabel);
-
-        // password entryfield
-        pwText = new JPasswordField(20);
-        pwText.setBounds(330, 20, 165, 25);
-        panel.add(pwText);
-
-        // sign in/up buttonsJLabel signInButton = new JButton("Sign in");
-        signInButton = new JButton("Sign in");
-        signInButton.setBounds(500, 20, 80, 25);
-        panel.add(signInButton);
-        signUpButton = new JButton("Sign up");
-        signUpButton.setBounds(500, 50, 80, 25);
-        panel.add(signUpButton);
-
-       /*
-            From here on out, we are dealing with the database: adding applications, price, numDownloads table
-        */
-
-        // set apps label
-        JLabel appsLabel = new JLabel("Applications");
-        appsLabel.setBounds(140, 50, 80, 25);
-        panel.add(appsLabel);
-        // create ScrollPane with scrollbar, set the table as what we're showing
-        // get a table with apps
-
-        JScrollPane scrollPane = new JScrollPane(conn.getAppsTable());
-        scrollPane.setBounds(10, 80, 350, 300 );
-        panel.add(scrollPane);
-        // create the text area that will display the description of the app
-        appDesc = new JTextArea("App: \n" + "Description: ");
-        appDesc.setBounds(370, 80, 300 , 300);
-        appDesc.setLineWrap(true);
-        appDesc.setWrapStyleWord(true);
-        panel.add(appDesc);
+     // Load App and User info
+        try { 
+        	Scanner scanner = new Scanner(new File("login.txt"));
+        	while(scanner.hasNextLine()) {
+        		String line = scanner.nextLine();
+        		String userAndPass[] = line.split(",");
+        		AOT.users.put(userAndPass[0],userAndPass[1]);
+        	}
+        	scanner.close();
+        } catch(FileNotFoundException e) {
+			System.out.println("login.txt file Not Found");
+		}
+        AOT.users.put(a1.username, a1.password);
+        User user1 = new User("testName", "testPassword");
+        AOT.users.put(user1.username, user1.password);
+        
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        
 
 
+        JTextField jtf = new JTextField("Username");
+        JTextField jpwf = new JTextField("Password");
+        JButton logInBttn = new JButton("Sign in");
+        JLabel lbl = new JLabel("Don't have an account yet?");
+        JButton signUpBtn = new JButton("Sign up");
+        JPanel statusPanel = new JPanel();
+        JPanel jp1 = new JPanel();
+        
+     // action listener for signUpBttn button
+ 		signUpBtn.addActionListener(new ActionListener() {
+ 			@Override
+ 			public void actionPerformed(ActionEvent e) {
+ 				String username = jtf.getText();
+ 				String password = jpwf.getText();
+ 				JFrame signupFrame = new JFrame("Signup");
+ 				signupFrame.setLayout(new FlowLayout());
+ 				User u = new User(username, password);
+ 				if (u.signUp(AOT)) {
+ 					JLabel success = new JLabel("Account created! Please click login.");
+ 					signupFrame.add(success);
+ 				} else {
+ 					JLabel fail = new JLabel("Account creation failed! You have already have a login account.");
+ 					signupFrame.add(fail);
+ 				}
+ 				jtf.setText("Username");
+ 				jpwf.setText("Password");
+ 				signupFrame.setSize(300, 60);
+ 				signupFrame.setVisible(true);
+ 			}
+ 		});
+ 		
+ 		
+ 	// action listener for login button
+ 			logInBttn.addActionListener(new ActionListener() {
+ 				@Override
+ 				public void actionPerformed(ActionEvent e) {
+ 					// read username and password text fields into string variables
+ 					String username = jtf.getText();
+ 					String password = jpwf.getText();
+ 					
+ 					JFrame loginFrame = new JFrame("Login");
+ 					loginFrame.setLayout(new FlowLayout());
+ 					
+ 					User u = new User(username, password);
+ 		
+ 						AOT.currentUser = u;
+ 						AOT.currentUser = new User("", "");
+ 						 
+ 						JLabel success = new JLabel("Login successful!");
+ 						loginFrame.add(success);
+ 						statusPanel.removeAll();
+ 						JLabel status = new JLabel("Currently logged in as User: " + username);
+ 						JButton logoutBtn = new JButton("Logout");
+ 						JFrame logoutFrame = new JFrame("Logout");
+ 						logoutFrame.setLayout(new FlowLayout());
+ 						logoutBtn.addActionListener(new ActionListener() {
+ 							@Override
+ 							public void actionPerformed(ActionEvent e) {
+ 								AOT.currentUser = new User("","");
+ 								JLabel logoutSuccess = new JLabel("Logout successful!");
+ 								logoutFrame.add(logoutSuccess);
+ 								logoutFrame.setSize(300, 60);
+ 								statusPanel.removeAll();
+ 								frame.setVisible(true);
+ 								logoutFrame.setVisible(true);
+ 							}
+ 						});
+ 						statusPanel.add(status);
+ 						statusPanel.add(logoutBtn);
+ 						frame.setVisible(true);
+ 				}
+ 			});
 
-        frame.setVisible(true);
+
+ 	    statusPanel.setLayout(new FlowLayout());
+        jp1.setLayout(new FlowLayout());
+        frame.setLayout(new BorderLayout());
+        frame.add(statusPanel, BorderLayout.NORTH);
+        frame.add(jp1, BorderLayout.CENTER);
+
+        statusPanel.add(jtf);
+	    statusPanel.add(jpwf);
+		statusPanel.add(logInBttn);
+		jp1.add(lbl);
+		jp1.add(signUpBtn);
+		
+		frame.setSize(600, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 
     }
 }
+
