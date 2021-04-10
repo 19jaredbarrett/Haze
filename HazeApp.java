d5d0d091f74db7453e18c4a52a1a19ee5a279545
19jaredbarrett <55817962+19jaredbarrett@users.noreply.github.com>
2021-04-10 12:06:06 -0400

    Added Complete db functionality to register user

--- HazeApp.java
+++ HazeApp.java
@@ -1,11 +1,11 @@
-import DbClasses.App;
-import DbClasses.ApplicationsTableModel;
+import DbClasses.User;
 
 import javax.swing.*;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.sql.SQLException;
+import java.util.Arrays;
 
 public class HazeApp {
     public static final SqlServerConnection conn  = new SqlServerConnection();
@@ -19,6 +19,9 @@ public class HazeApp {
     public static JTextArea appDesc;
     public static JTable appsTable;
     public static JScrollPane scrollPane;
+
+    private static User currentUser;
+
     public static void main(String[] args) throws SQLException {
 
         panel = new JPanel();
@@ -26,14 +29,50 @@ public class HazeApp {
         frame.setSize(720,640);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
-        createHomePage();
+        displayHomePage(false);
         frame.setVisible(true);
 
     }
-    public static void createHomePage() throws SQLException {
+
+    /**
+     * Displays the main page of the app
+     * @throws SQLException throws a sql exception
+     */
+    public static void displayHomePage(boolean loggedIn) throws SQLException {
         panel.setLayout(null);
         clearPanel();
+        if(loggedIn)
+            displayLoggedInInterface(conn.getCurrentUser().getUsername());
+        else
+            displayGuestInterface();
+       /*
+            From here on out, we are dealing with the database: adding applications, price, numDownloads table
+        */
 
+        // set apps label
+        JLabel appsLabel = new JLabel("Applications");
+        appsLabel.setBounds(140, 50, 80, 25);
+        panel.add(appsLabel);
+        // create ScrollPane with scrollbar, set the table as what we're showing
+        // get a table with apps
+        appsTable = conn.getAppsTable();
+        scrollPane = new JScrollPane(appsTable);
+        scrollPane.setBounds(10, 80, 350, 300 );
+        panel.add(scrollPane);
+        // create the text area that will display the description of the app
+        appDesc = new JTextArea("App: \n" + "Description: ");
+        appDesc.setBounds(370, 80, 300 , 300);
+        appDesc.setLineWrap(true);
+        appDesc.setWrapStyleWord(true);
+        panel.add(appDesc);
+    }
+
+    private static void displayLoggedInInterface(String username) {
+        JLabel userLabel = new JLabel("Logged in as: " + username);
+        userLabel.setSize(100, 40);
+        userLabel.setBounds(10, 20, 100, 40);
+    }
+    private static void displayGuestInterface()  {
         // username label
         JLabel userLabel = new JLabel("Username:");
         userLabel.setBounds(10, 20, 80, 25);
@@ -67,29 +106,7 @@ public class HazeApp {
         signInButton.addActionListener(signInHandler);
         SignUpActionHandler signUpHandler = new SignUpActionHandler();
         signUpButton.addActionListener(signUpHandler);
-
-       /*
-            From here on out, we are dealing with the database: adding applications, price, numDownloads table
-        */
-
-        // set apps label
-        JLabel appsLabel = new JLabel("Applications");
-        appsLabel.setBounds(140, 50, 80, 25);
-        panel.add(appsLabel);
-        // create ScrollPane with scrollbar, set the table as what we're showing
-        // get a table with apps
-        appsTable = conn.getAppsTable();
-        scrollPane = new JScrollPane(appsTable);
-        scrollPane.setBounds(10, 80, 350, 300 );
-        panel.add(scrollPane);
-        // create the text area that will display the description of the app
-        appDesc = new JTextArea("App: \n" + "Description: ");
-        appDesc.setBounds(370, 80, 300 , 300);
-        appDesc.setLineWrap(true);
-        appDesc.setWrapStyleWord(true);
-        panel.add(appDesc);
     }
-
     /**
      * Function: clear the Jframe AKA make blank
      */
@@ -105,7 +122,7 @@ public class HazeApp {
      * to create a username and password.
      * This also will insert username and password into database.
      */
-    public static void makeCreateAccountInterface() {
+    public static void displayCreateAccountInterface() throws SQLException {
         // clear the jframe
         clearPanel();
 
@@ -130,9 +147,9 @@ public class HazeApp {
         panel.add(pwText);
 
         // label confirm password
-        JLabel confPass = new JLabel("Confirm Password:");
-        confPass.setBounds(200, 50, 150, 25);
-        panel.add(confPass);
+        JLabel confPassLabel = new JLabel("Confirm Password:");
+        confPassLabel.setBounds(200, 50, 150, 25);
+        panel.add(confPassLabel);
 
         // confirm the password
         JPasswordField confPassField = new JPasswordField(20);
@@ -142,9 +159,47 @@ public class HazeApp {
 
 
         //sign up button
-        signUpButton = new JButton("Sign up");
-        signUpButton.setBounds(500, 20, 80, 25);
-        panel.add(signUpButton);
+        JButton registerUser = new JButton("Register");
+        registerUser.setBounds(500, 20, 80, 25);
+        // mouse Listener: creates user based on several conditions:
+        // user doesn't exist, passwords are equal, username provided
+        registerUser.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent evt) {
+                String user = userText.getText();
+                char[] pass = pwText.getPassword();
+                char[] confPass = confPassField.getPassword();
+                String displayString;
+                if(user.isEmpty()|| pass.length == 0 || confPass.length == 0) {
+                    displayString = "*must provide username and password*";
+                } else if (Arrays.equals(pass, confPass)) {
+                    boolean isCreated = false;
+                    try {
+                        isCreated = conn.registerUser(user, pass);
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+                    if(!isCreated) {
+                        displayString = "*User already exists*";
+                    } else {
+                        displayString = "!!!!User created Successfully!!!!";
+                    }
+                } else {
+                    displayString = "*Must match passwords*";
+                }
+                // display the home page with the label
+                try {
+                    displayHomePage(false);
+                    JLabel displayCreatedSuccess = new JLabel(displayString);
+                    displayCreatedSuccess.setBounds(250, 50, 300, 25);
+                    displayCreatedSuccess.setForeground(Color.BLUE);
+                    panel.add(displayCreatedSuccess);
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            } // end method mouseClicked
+        });
+        panel.add(registerUser);
 
         //home button
         JButton homeButton = new JButton("Home");
@@ -154,7 +209,7 @@ public class HazeApp {
             @Override
             public void mouseClicked(MouseEvent evt) {
                 try {
-                    createHomePage();
+                    displayHomePage(false);
                 } catch (SQLException throwables) {
                     throwables.printStackTrace();
                 }
@@ -165,19 +220,11 @@ public class HazeApp {
         frame.invalidate();
         frame.repaint();
     }
-    /*public static void CreateUserInterface(String user) {
-        userPanel = new JPanel();
 
-        // username label at head of profile page
-        JLabel userLabel = new JLabel("Welcome " + user + "!");
-        userLabel.setBounds(10, 20, 80, 25);
-        userPanel.add(userLabel);
-        frame.setVisible(true);
-    }*/
 
     /**
      * updates user interface
-     * @param user
+     * @param user the user we want to log in
      */
     public static void updateUserInterfaceLoggedIn(String user) {
         panel.remove(0);
@@ -191,7 +238,7 @@ public class HazeApp {
             @Override
             public void mouseClicked(MouseEvent evt) {
                 try {
-                    createHomePage();
+                    displayHomePage(false);
                 } catch (SQLException throwables) {
                     throwables.printStackTrace();
                 }
@@ -203,16 +250,13 @@ public class HazeApp {
         frame.repaint();
     }
 
-    public static void login(String username) {
-        updateUserInterfaceLoggedIn(username);
-
+    public User getCurrentUser() {
+        return currentUser;
     }
 
-    public static void signUp() {
-        clearPanel();
-        makeCreateAccountInterface();
-
-        SignUpActionHandler signUpHandler = new SignUpActionHandler();
-        signUpButton.addActionListener(signUpHandler);
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
     }
+
+
 }
--- SqlServerConnection.java
+++ SqlServerConnection.java
@@ -1,6 +1,7 @@
 import DbClasses.App;
 import DbClasses.ApplicationsTableModel;
 import DbClasses.ConnectionProvider;
+import DbClasses.User;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -16,8 +17,8 @@ public class SqlServerConnection implements ConnectionProvider {
     private static final HikariConfig conf = new HikariConfig();
     private static HikariDataSource ds;
     private static Connection conn;
-    private String currentDescDisplayed;
-    private ApplicationsTableModel model;
+    private User currentUser;
+
     //HazePa$$word123
     public SqlServerConnection ()  {
         conf.setJdbcUrl("jdbc:sqlserver://localhost\\\\SQLEXPRESS:1433;databaseName=Apps201;");
@@ -38,15 +39,12 @@ public class SqlServerConnection implements ConnectionProvider {
     }
 
 
+
+
     /**
      *
-     * @return
-     * @throws SQLException
-     */
-    /**
-     *
-     * @return
-     * @throws SQLException
+     * @return returns an apps table
+     * @throws SQLException if the statement fucks up
      */
     public JTable getAppsTable() throws SQLException {
         conn = getConnection();
@@ -155,4 +153,44 @@ public class SqlServerConnection implements ConnectionProvider {
         return appArr;
     }
 
+    /**
+     * Registers a user into the system if it exists
+     * @param username
+     * @return
+     * @throws SQLException
+     */
+    public boolean registerUser(String username, char[] pass) throws SQLException {
+        conn = getConnection();
+        User u = new User(username, pass);
+        boolean isSuccess = false;
+        String call = "{call registerUser(?, ?, ?, ?)}";
+        try(CallableStatement stmt = conn.prepareCall(call)) {
+            // set order and isAsc to true
+            stmt.setString(1, u.getUsername());
+            stmt.setString(2, u.getPassword());
+            stmt.setFloat(3, (float)u.getBalance());
+            stmt.setInt(4, u.getAccessLevel());
+            boolean hasResult = stmt.execute();
+            if(hasResult) {
+                ResultSet rs = stmt.getResultSet();
+                if(rs.next()) {
+                    isSuccess = true;
+                    setCurrentUser(new User(username, pass));
+                }
+            }
+            stmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return isSuccess;
+    }
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
 }
