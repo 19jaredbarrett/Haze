995200e42bfae9769adef466518b23542eefdebd
Qi Feng <49265120+Rivendell-FQ@users.noreply.github.com>
2021-04-23 16:26:37 -0400

    admin and moderator
    admin and moderator

--- Applications/src/main/java/DbClasses/Admin.java
+++ Applications/src/main/java/DbClasses/Admin.java
@@ -2,7 +2,7 @@ package DbClasses;
 
 public class Admin extends Moderator {
 
-	public Admin(String username, String password, int balance,int accessLevel) {
+	public Admin(String username, String password, double balance,int accessLevel) {
 		super(username, password,balance,accessLevel);
 		setAccessLevel(2);
 	}
--- Applications/src/main/java/DbClasses/Moderator.java
+++ Applications/src/main/java/DbClasses/Moderator.java
@@ -2,7 +2,7 @@ package DbClasses;
 
 public class Moderator extends User {
 
-	public Moderator(String username, String password, int balance,int accessLevel) {
+	public Moderator(String username, String password, double balance,int accessLevel) {
 		super(username, password,balance,accessLevel);
 		setAccessLevel(1);
 	}
