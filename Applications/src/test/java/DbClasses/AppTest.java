f48441788e0aabd679c30fcc86c94335eec105f4
Qi Feng <49265120+Rivendell-FQ@users.noreply.github.com>
2021-04-29 08:24:09 -0400

    delete repeat code
    delete repeat code

--- Applications/src/test/java/DbClasses/AppTest.java
+++ Applications/src/test/java/DbClasses/AppTest.java
@@ -4,19 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
-
     App ex = new App(1, "DogeGame", "You play as a doge!", 50.00, 50504, 0);
     App ds = new App(2, "DarkSouls", "A very simple third-person action game of soul system. Very simple, the enemies inside are easy.", 49.00, 1000000, 10);
     App dota = new App(3, "Dota2", "A MOBA 5V5 game that has been popular all over the world for decades. You can experience all champions for free.", 0, 5000000, 10);
     App trash = new App(4, "TrashGame", "A deceptive junk game. It is not recommended to buy.", 1, 123, 0);
 
-
-
     @org.junit.jupiter.api.Test
     void setId() {
         ex.setId(2);
         assertEquals(2, ex.getId());
-
         
         ds.setId(2);
         assertEquals( 2,  ds.getId());
@@ -24,14 +20,6 @@ class AppTest {
         dota.setId(5);
         assertEquals( 5,  dota.getId());
         
-
-
-        ds.setId(2);
-        assertEquals( 2,  ds.getId());
-
-        dota.setId(5);
-        assertEquals( 5,  dota.getId());
-
         trash.setId(10);
         assertEquals(10 ,  trash.getId());
     }
@@ -40,21 +28,12 @@ class AppTest {
     void setAppName() {
         ex.setAppName("Doge");
         assertEquals("Doge", ex.getAppName());
-
         
         ds.setAppName("Dark Souls 3");
         assertEquals("Dark Souls 3", ds.getAppName());
         
         dota.setAppName("Doooooota2");
         assertEquals("Doooooota2", dota.getAppName());
-
-
-        ds.setAppName("Dark Souls 3");
-        assertEquals("Dark Souls 3", ds.getAppName());
-
-        dota.setAppName("Doooooota2");
-        assertEquals("Doooooota2", dota.getAppName());
-
 
         trash.setAppName("trash");
         assertEquals("trash", trash.getAppName());
@@ -70,15 +49,6 @@ class AppTest {
         
         dota.setPrice(-1);
         assertEquals(-1,dota.getPrice());
-        
-
-
-        ds.setPrice(20);
-        assertEquals(20,ds.getPrice());
-
-        dota.setPrice(-1);
-        assertEquals(-1,dota.getPrice());
-
 
         trash.setPrice(10000);
         assertEquals(10000,trash.getPrice());
@@ -96,11 +66,6 @@ class AppTest {
         dota.setNumDownloads(dota.getNumDownloads()+1);
         assertEquals(5000001, dota.getNumDownloads());
 
-        ds.setNumDownloads(ds.getNumDownloads()+1);
-        assertEquals(1000001, ds.getNumDownloads());
-
-        dota.setNumDownloads(dota.getNumDownloads()+1);
-        assertEquals(5000001, dota.getNumDownloads());
         trash.setNumDownloads(trash.getNumDownloads()+1);
         assertEquals(124, trash.getNumDownloads());
     }
@@ -158,15 +123,6 @@ class AppTest {
         ds.setDescription("a very hard game");
         assertEquals("a very hard game",ds.getDescription());
         
-        dota.setDescription("moba game");
-        assertEquals("moba game",dota.getDescription());
-        
-        trash.setDescription("really trash game");
-        assertEquals("really trash game",trash.getDescription());
-
-        ds.setDescription("a very hard game");
-        assertEquals("a very hard game",ds.getDescription());
-
         dota.setDescription("moba game");
         assertEquals("moba game",dota.getDescription());
 
